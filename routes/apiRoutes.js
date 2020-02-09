ef2ccd4272f4a046e103446933413397b8f2b086
aarkitekkt <johnson.brycej@gmail.com>
2020-02-09 09:53:45 -0700

    push new note to db and write new file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var notes = require("../db/db.json");
+var fs = require("fs");
 
 module.exports = function (app) {
 
@@ -13,6 +14,31 @@ module.exports = function (app) {
 
         console.log(newNote);
 
-        notes.push(newNote);
-    })
-}
+        fs.readFile("C:/Users/johns/Developer/uofu/sandbox/homework/note-taker/db/db.json", "utf8", function (err, notes) {
+
+            if (err) {
+                console.log(err);
+            } else {
+
+                notes = JSON.parse(notes);
+
+                notes.push(newNote);
+
+                console.log(notes);
+
+                fs.writeFile("C:/Users/johns/Developer/uofu/sandbox/homework/note-taker/db/db.json", JSON.stringify(notes), function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+
+                    console.log("added " + JSON.stringify(newNote) + " to db.json");
+                })
+
+
+            }
+
+        })
+    });
+};
+
+
